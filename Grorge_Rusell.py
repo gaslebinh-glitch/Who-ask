289110f775ae3f3ea36e773a1681722eb8271281
gaslebinh-glitch <gaslebinh@gmail.com>
2026-02-21 11:59:49 +0700

    Nothing

--- Grorge_Rusell.py
+++ Grorge_Rusell.py
@@ -1,9 +1,8 @@
 import streamlit as st
 st.set_page_config(page_title="YOASOBI", page_icon=":musical_note:")
 image='https://namu.wiki/w/YOASOBI?uuid=08c2038b-cf0c-475a-b0f8-3b16e0d0198b'
-sound=open("Blue _ 群青.mp3","rb")
 st.title("Bài hát yêu thích của tôi: YOASOBI - Blue _ 群青")
-st.audio(sound, format="audio/mp3")
+st.audio("Blue _ 群青.mp3", format="audio/mp3")
 st.title("Video âm nhạc của YOASOBI - Blue _ 群青")
 video='https://www.youtube.com/watch?v=Y4nEEZwckuU'
 st.video(video)
