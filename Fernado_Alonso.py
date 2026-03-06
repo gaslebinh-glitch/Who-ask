8c74205a8af072ba427b2572588eabf018d3ccd8
gaslebinh-glitch <gaslebinh@gmail.com>
2026-03-06 20:50:55 +0700

    Hello World!

--- Fernado_Alonso.py
+++ Fernado_Alonso.py
@@ -1,98 +1,49 @@
-import pygame, sys
-import random
-
-pygame.init()
-
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
-pygame.display.set_caption('Need for Speed')
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
-BLACK = (0, 0, 0)
-RED = (255, 0, 0)
-BLUE = (0, 0, 255)
-GREEN = (0, 255, 0)
-WHITE = (255, 255, 255)
-PLAYER_SIZE = 50
-ENEMY_SIZE = 30
-PLAYER_POS = [SCREEN_WIDTH // 2, SCREEN_HEIGHT - PLAYER_SIZE]
-ENEMY_COUNT = 5
-PLAYER_SPEED = 10
-ENEMY_SPEED = 3
-enemy_list = []
-score = 0
-lives = 3 
-
-
-for i in range(ENEMY_COUNT):
-    enemy_pos = [random.randint(0, SCREEN_WIDTH - ENEMY_SIZE), random.randint(-SCREEN_HEIGHT - ENEMY_SIZE * 2, 0)]
-    enemy_list.append(enemy_pos)
-    
-def draw_object():
-    screen.fill(BLACK)
-    # vẽ người chơi
-    pygame.draw.rect(screen, WHITE, [PLAYER_POS[0], PLAYER_POS[1], PLAYER_SIZE, PLAYER_SIZE])
-    # vẽ enemy
-    for enemy_pos in enemy_list:
-        pygame.draw.rect(screen, RED, [enemy_pos[0], enemy_pos[1], ENEMY_SIZE, ENEMY_SIZE])
-        
-def update_enemy():
-    global score
-    for i in range(len(enemy_list)):
-        if enemy_list[i][1] < SCREEN_HEIGHT:
-            enemy_list[i][1] += ENEMY_SPEED
+import streamlit as st
+st.title("Đăng kí vào Long's App")
+list_sex=["Nam","Nữ","Khác"]
+list_hobbies=["Đá bóng","Bóng rổ","Bơi lội","Chạy bộ","Đạp xe","Khác"]
+data_tai=''
+with st.form(key="str", clear_on_submit=False):
+    name=st.text_input("Họ và tên")
+    age=st.slider("Tuổi", min_value=1, max_value=99, step=1)
+    dia_chi=st.text_input("Địa chỉ")
+    email=st.text_input("Email")
+    sdt=st.text_input("Số điện thoại")
+    sex=st.selectbox("Giới tính", list_sex)
+    hobbies=st.multiselect("Sở thích", list_hobbies)
+    chap_thuan=st.checkbox("Tôi đồng ý với các điều khoản và điều kiện")
+    submit_button=st.form_submit_button(label="Đăng kí")
+    if submit_button:
+        if not name:
+            st.error("Vui lòng nhập họ và tên.")
+        elif not dia_chi:
+            st.error("Vui lòng nhập địa chỉ.")
+        elif not email:
+            st.error("Vui lòng nhập email.")
+        elif not sdt:
+            st.error("Vui lòng nhập số điện thoại.")    
+        elif not hobbies:
+            st.error("Vui lòng chọn ít nhất một sở thích.")
+        elif not sex:
+            st.error("Vui lòng chọn giới tính.")
+        elif not chap_thuan:
+            st.error("Vui lòng đồng ý với các điều khoản và điều kiện.")
         else:
-            score += 1
-            enemy_list[i][0] = random.randint(0, SCREEN_WIDTH - ENEMY_SIZE)
-            enemy_list[i][1] = random.randint(-SCREEN_HEIGHT - ENEMY_SIZE * 2, 0)
-
-def handle_event():
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            return True
-    
-    key = pygame.key.get_pressed()
-    
-    if key[pygame.K_LEFT] and PLAYER_POS[0] > 0:
-        PLAYER_POS[0] -= PLAYER_SPEED
-    if key[pygame.K_RIGHT] and PLAYER_POS[0] < SCREEN_WIDTH - PLAYER_SIZE:
-        PLAYER_POS[0] += PLAYER_SPEED
-    
-    return False
-
-def detect_collision(obj1_pos, obj2_pos):
-    obj1_x, obj1_y = obj1_pos[0], obj1_pos[1]
-    obj2_x, obj2_y = obj2_pos[0], obj2_pos[1]
-    
-    if (obj1_x >= obj2_x and obj1_x < (obj2_x + ENEMY_SIZE)) or (obj2_x >= obj1_x and obj2_x < (obj1_x + PLAYER_SIZE)):
-        if (obj1_y >= obj2_y and obj1_y < (obj2_y + ENEMY_SIZE)) or (obj2_y >= obj1_y and obj2_y < (obj1_y + PLAYER_SIZE)):
-            return True
-    return False
-def check_collision():
-    global lives    
-    enemies_to_reset = []
-    for i in range(len(enemy_list)):
-        enemy_pos = enemy_list[i]
-        if detect_collision(PLAYER_POS, enemy_pos):
-            lives -= 1 
-            enemies_to_reset.append(i)    
-        for i in range(len(enemy_list)):
-            if i in enemies_to_reset:
-               enemy_list[i][0] = random.randint(0, SCREEN_WIDTH - ENEMY_SIZE)
-               enemy_list[i][1] = random.randint(-SCREEN_HEIGHT - ENEMY_SIZE * 2, 0)
-    
-game_over = False
-while not game_over:
-    game_over = handle_event()
-    update_enemy()
-    check_collision()
-    if lives <= 0:
-        game_over = True
-    draw_object()
-    font = pygame.font.SysFont("arial", 35)
-    score_text = font.render("Score: " + str(score), True, GREEN)
-    screen.blit(score_text, (10, 10))
-    lives_text = font.render("Lives: " + str(lives), True, BLUE)
-    screen.blit(lives_text, (10, 50))
-    pygame.display.update()
+            st.success("Đăng kí thành công!")
+            st.write("Thông tin của bạn:")
+            st.write("Họ và tên:", name)
+            st.write("Tuổi:", age)
+            st.write("Địa chỉ:", dia_chi)
+            st.write("Email:", email)
+            st.write("Số điện thoại:", sdt)
+            st.write("Giới tính:", sex)
+        data_tai=f"""
+            Họ và tên: {name}
+            Tuổi: {age} 
+            Địa chỉ: {dia_chi}
+            Email: {email}  
+            Số điện thoại: {sdt}
+            Giới tính: {sex}
+            Sở thích: {', '.join(hobbies)}  
+        """
+st.download_button(label="Tải thông tin của bạn", data=data_tai, file_name="")
